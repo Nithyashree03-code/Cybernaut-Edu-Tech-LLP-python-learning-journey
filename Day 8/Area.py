097ad509e5235396e90b7842565dcd71b3c3c16e
Nithyashree03-code <nshree03112005@gmail.com>
2024-10-14 18:43:09 +0530

    Assignment added

---
+++ Day 8/Area.py
@@ -0,0 +1,4 @@
+def calculate_area(width, height):
+    return width * height
+area = calculate_area(6, 8)
+print("Area of the rectangle:", area)
---
+++ Day 8/Key.py
@@ -0,0 +1,4 @@
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+print_kwargs(name="Nithya", age=19, city="Madurai")
